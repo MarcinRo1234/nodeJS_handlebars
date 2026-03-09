a5b4dd7a06a9865f8159ccd8bd099d8d1069ac5d
Marcin Rogulski <Marcinos.Rogulski@gmail.com>
2026-03-09 01:20:43 +0100

    podstawy handlebars

---
+++ app.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
+
+const app = express();
+
+app.engine('handlebars', expressHbs()) // zamiast handlebars można wpisa c coss innego i potem to jest rozszerzenie do pliku
+app.set('view engine', 'handlebars'); // zamiast handlebars można wpisa c coss innego i potem to jest rozszerzenie do pliku
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+});
+
+app.listen(3000);
