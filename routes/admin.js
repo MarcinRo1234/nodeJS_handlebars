615204a896bdb71eff7dd6b510f6ac3ed1ec60ab
Marcin Rogulski <Marcinos.Rogulski@gmail.com>
2026-03-09 21:41:27 +0100

    dodanie MVC

--- routes/admin.js
+++ routes/admin.js
@@ -2,27 +2,20 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
+// const rootDir = require('../util/path'); niepotrzebne - folder util też można usunąć
 
+const productsController = require('../controllers/products');
 const router = express.Router();
 
-const products = [];
+// const products = []; usuniete na potrzeby MVC
 
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-  // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-<<<<<<< HEAD
-  res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product', formsCSS: true, produtCSS: true, activeAddProduct: true});
-=======
-  res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product'});
->>>>>>> a5b4dd7a06a9865f8159ccd8bd099d8d1069ac5d
-});
+router.get('/add-product', productsController.getAddProduct);
+
 
 // /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect('/');
-});
+router.post('/add-product', productsController.postAddProduct);
 
-exports.routes = router;
-exports.products = products;
+// exports.routes = router;
+// exports.products = products;
+module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,30 +1,13 @@
 const path = require('path');
 
 const express = require('express');
+const productsController = require('../controllers/products')
 
-const rootDir = require('../util/path');
-const adminData = require('./admin');
+// const rootDir = require('../util/path');
+// const adminData = require('./admin');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  const products = adminData.products;
-<<<<<<< HEAD
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/',
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true,
-    layout: false
-  });
-});
+router.get('/', productsController.getProducts);
 
 module.exports = router;
-=======
-  res.render('shop', {prods: products, pageTitle: 'Shop', path: '/', hasProducts: products.length > 0});
-});
-
-module.exports = router;
->>>>>>> a5b4dd7a06a9865f8159ccd8bd099d8d1069ac5d
